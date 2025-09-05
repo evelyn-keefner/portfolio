c06961d222e01681f64c002a6fbfd9182b82ad12
evelyn-keefner <evelyn0keefner@gmail.com>
2025-09-05 15:12:12 -0400

    blossoms ai does not load .

--- ch01/exercises/data.py
+++ ch01/exercises/data.py
@@ -1,7 +1,8 @@
 print("10 x 5:", 10 * 5) # Expected answer: 50
 print("10 ** 5:", 10 ** 5) # Expected answer: 100000
 print("15 / 2:", 15 / 2) # Expected answer: 7.5
-print("-15 // 10:", -15 // 10) # Expected answer: 0 | actual answer: -2
+print("15 // 10:", 15 // 10) # Expected answer: 1
+print("-15 // 10:", -15 // 10) # Expected answer: 0 | actual answer: -2, it works backwards I'm guessing? 
 print("15 % 10:", 15 % 10) # Expected answer: 5
 print("10 % 15:", 10 % 15) # Expected answer: 10
 print("10 % 10:", 10 % 10) # Expected answer: 0
--- ch01/exercises/rectangle.py
+++ ch01/exercises/rectangle.py
@@ -2,6 +2,8 @@ shape = 'hexagon'
 sides = 6
 
 internal_angle_sum = (sides - 2) * 180
+external_angle_sum = 360 / sides
 angle_sum = internal_angle_sum / sides
 
-print("The shape", shape, "has side angles of", angle_sum, "degrees.")
+print("The shape", shape, "has internal angles which measure", angle_sum, "degrees.")
+print("The shape " + shape + " has external angles which measure " + str(external_angle_sum) + " degrees.")
