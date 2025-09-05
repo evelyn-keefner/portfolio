6a439fd97c65156155a4ceb03bdfa09cb121d2dd
evelyn-keefner <evelyn0keefner@gmail.com>
2025-09-05 14:54:04 -0400

    CH01 Exercise data.py update

--- ch01/exercises/data.py
+++ ch01/exercises/data.py
@@ -1,9 +1,9 @@
-print("Expected answer: ", 10 * 5) # 50
-print("Expected answer: ", 10 ** 5) # 100000
-print("Expected answer: ", 15 / 2) # 7.5
-print("Expected answer: ", -15 // 10) # 0 | actual answer: -2
-print("Expected answer: ", 15 % 10) # 5
-print("Expected answer: ", 10 % 15) # 10
-print("Expected answer: ", 10 % 10) # 0
-print("Expected answer: ", 0 % 10) # 0 
-print("Expected answer: ", 10 / 15) # 0.667 | actual answer: 0.666666666666666 not "mathemetically" correct because the 6's are repeating and do not stop, and here that is not the case so some precision is missing out on this calculation'
+print("10 x 5:", 10 * 5) # Expected answer: 50
+print("10 ** 5:", 10 ** 5) # Expected answer: 100000
+print("15 / 2:", 15 / 2) # Expected answer: 7.5
+print("-15 // 10:", -15 // 10) # Expected answer: 0 | actual answer: -2
+print("15 % 10:", 15 % 10) # Expected answer: 5
+print("10 % 15:", 10 % 15) # Expected answer: 10
+print("10 % 10:", 10 % 10) # Expected answer: 0
+print("0 % 10:", 0 % 10) # Expected answer: 0
+print("10 / 15:", 10 / 15) # Expected answer: 0.667 | actual answer: 0.666666666666666 not "mathemetically" correct because the 6's are repeating and do not stop, and here that is not the case so some precision is missing out on this calculation'
