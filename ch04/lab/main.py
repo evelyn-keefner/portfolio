8488e56de9cc28f7940f05078c44dc64b4798c06
evelyn-keefner <evelyn0keefner@gmail.com>
2025-09-26 10:45:59 -0400

    lab 4

---
+++ ch04/lab/main.py
@@ -0,0 +1,77 @@
+import target
+import turtle
+
+def simulate_game(pen, sizes, rounds, guess) -> int:
+    player1_score = 0
+    player2_score = 0
+    score_writer = turtle.Turtle()
+    score_writer.speed(10)
+    score_writer.color("white")
+
+    for i in range(rounds):
+        if (target.shoot_dart(pen, sizes, "green")):
+            player1_score += 1
+            print(f"player 1: {player1_score}")
+        
+        if (target.shoot_dart(pen, sizes, "red")):
+            player2_score += 1
+            print(f"player 2: {player2_score}")
+        
+        score_writer.clear()
+        score_writer.penup()
+        score_writer.goto(-sizes[0], sizes[1])
+        score_writer.pendown()
+        score_writer.write(f"Player 1 (Green): {player1_score}", align="left", font=('Arial', 24, 'normal'))
+        score_writer.penup()
+        score_writer.goto(sizes[0], sizes[1])
+        score_writer.pendown()
+        score_writer.write(f"Player 2 (Red): {player2_score}", align="right", font=('Arial', 24, 'normal'))
+        score_writer.penup()
+
+    score_writer.goto(0, 0)
+    score_writer.pendown()
+    if (player1_score > player2_score):
+        if (guess == 1):
+            score_writer.write("You guessed the winner correctly!", align="center", font=('Arial', 22, 'normal'))
+        else:
+            score_writer.write("You didn't guess the winner this time.", align="center", font=('Arial', 22, 'normal'))
+        return 1 # player 1 wins
+    elif (player2_score > player1_score):
+        if (guess == 2):
+            score_writer.write("You guessed the winner correctly!", align="center", font=('Arial', 22, 'normal'))
+        else:
+            score_writer.write("You didn't guess the winner this time.", align="center", font=('Arial', 22, 'normal'))
+        return 2 # player 2 wins
+    score_writer.write("It was a tie!", align="center", font=('Arial', 22, 'normal'))
+    return 0 # in case of tie
+
+def betting(window):
+    input = window.textinput("NIM", "Which player do you think will win? Green, or Red?").lower()
+    while ((input != "green") and (input != "red")):
+        input = window.textinput("NIM", "Sorry that wasn't a valid option. Please type either Green or Red.").lower()
+
+    if (input == "green"):
+        return 1
+    else:
+        return 2
+
+def main():
+    wn = turtle.Screen() 
+    wn.bgcolor("blue")
+    # makes screen into square
+    sizes = wn.screensize()
+    if (sizes[0] < sizes[1]):
+        wn.screensize(sizes[0], sizes[0])
+    else:
+        wn.screensize(sizes[1], sizes[1])
+    sizes = wn.screensize()
+    pen = turtle.Turtle()
+    pen.speed(10)
+    
+    target.draw_target(pen, sizes)
+    simulate_game(pen, sizes, 10, betting(wn))
+
+    wn.exitonclick()
+
+if __name__ == "__main__":
+    main()
---
+++ ch04/lab/target.py
@@ -0,0 +1,124 @@
+import turtle
+import random
+import math
+
+'''
+draws a circle target the size of the screen 
+@param 
+    pen: turtle.Turtle()
+        the pen the function uses to draw the circle
+    sizes: int[]
+        array of sizes, [0] is width [1] is height of screen
+'''
+def draw_target(pen, sizes):
+    pen.color("pink")
+    pen.penup()
+    pen.right(90)
+    pen.forward(sizes[0])
+    pen.left(90)
+    pen.pendown()
+    pen.begin_fill()
+    pen.circle(sizes[0])
+    pen.end_fill()
+    
+    pen.color("black")
+    pen.left(90)
+    pen.forward(sizes[0] * 2)
+    pen.left(180)
+    pen.forward(sizes[0])
+    pen.left(90)
+    pen.forward(sizes[0])
+    pen.left(180)
+    pen.forward(sizes[0] * 2)
+    
+    pen.penup()
+    pen.home()
+    pen.right(90)
+    pen.forward(sizes[0])
+    pen.left(90)
+    pen.pensize(5)
+    pen.color("black")
+    pen.pendown()
+    pen.circle(sizes[0])
+    pen.penup()
+
+'''
+returns whether or not a coordinate is within a circle based on the radius of the cirlce
+@param 
+     x: int 
+        x coordinate of position
+     y: int 
+        y coordinate of position
+     radius: int
+        radius of circle
+@returns bool 
+    True if the cooridnate is on target 
+    False if not
+'''
+def dart_on_target(x, y, radius):
+    if (math.hypot(x, y) > radius):
+        return False
+    return True
+
+'''
+returns two random coordinates given the width and height of a screen
+@param 
+    sizes: int[]
+    an array containing the width and height of a screen, index 0 being width, index 1 being height
+@returns int[]
+    an array with index 0 being an x coordinate and index 1 being a y coordinate
+'''
+def dart_coordinates(sizes):
+    x = random.randrange(int(-sizes[0]), int(sizes[0]))
+    y = random.randrange(int(-sizes[1]), int(sizes[1]))
+    return [x, y]
+
+'''
+randomly places dots onto a screen given the screen's width and height 
+@param 
+    pen: turtle.Turtle()
+        the turtle element that will be used to draw the points
+    sizes: int[]
+        an array for screen size in which index 0 contains screen width and index 1 contains screen height
+@returns bool 
+    true if shot hit
+    false if shot didn't hit
+'''
+def shoot_dart(pen, sizes, color) -> bool:
+    hit = False
+    shot = dart_coordinates(sizes)
+    pen.goto(shot[0], shot[1])
+    if (dart_on_target(shot[0], shot[1], sizes[0])):
+        pen.color(color)
+        hit = True
+    else:
+        pen.color("grey")
+        hit = False
+    pen.pendown()
+    pen.dot(5)
+    pen.penup()
+    return hit
+
+def main():
+    # screen and turtle setup
+    wn = turtle.Screen() 
+    wn.bgcolor("blue")
+    # makes screen into square
+    sizes = wn.screensize()
+    if (sizes[0] < sizes[1]):
+        wn.screensize(sizes[0], sizes[0])
+    else:
+        wn.screensize(sizes[1], sizes[1])
+    sizes = wn.screensize()
+    
+    pen = turtle.Turtle()
+    pen.speed(10)
+    
+    draw_target(pen, sizes)
+    for i in range(40):
+        shoot_dart(pen, sizes, "green")
+    
+    wn.exitonclick()
+
+if __name__ == "__main__":
+    main()
