090d5c78873de060cb68507e241589555915c3a8
evelyn-keefner <evelyn0keefner@gmail.com>
2025-10-10 15:35:38 -0400

    ch05 exercises updates

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,18 +1,32 @@
-def star_pyramid(rows):
-    pyramid = ''
+'''
+prints an increasing number of stars
+@param
+    rows : int
+        The number of rows to be printed
+@returns : None
+'''
+def star_pyramid(rows) -> None:
+    pyramid: str = ''
     for i in range(1, (rows + 1)):
         pyramid += '*'
         print(pyramid)
     return None
 
-def rstar_pyramid(rows):
-    pyramid = '*' * rows
+'''
+prints a decreasing number of stars
+@param
+    rows : int
+        The number of rows to be printed
+@returns : None
+'''
+def rstar_pyramid(rows) -> None:
+    pyramid: str = '*' * rows
     for i in range((rows), 0, -1):
         pyramid = pyramid[0:i]
         print(pyramid)
     return None
 
-def main():
+def main() -> None:
     rows = int(input("How many rows?\n"))
     star_pyramid(rows)
     rstar_pyramid(rows)
