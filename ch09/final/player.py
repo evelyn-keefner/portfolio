fa50c16b523712bc8b9f7a3fa7559c98f02c3539
evelyn-keefner <evelyn0keefner@gmail.com>
2025-10-28 19:07:33 -0400

    updates to final

--- ch09/final/player.py
+++ ch09/final/player.py
@@ -4,7 +4,7 @@ class Player(pygame.sprite.Sprite): # class inherits from the sprite class to be
     def __init__(self, pos, group):
         super().__init__(group) # adds player sprite into the camera_group sprite group calling the sprite constructor Sprite(self, group)
         self.image = pygame.image.load('sprites/placeholder_sprites/placeholder_character.png')
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(center = pos) # image_group needs an image and a rect in order to use it's built in functions draw() and update()
         self.direction = pygame.math.Vector2()
         self.velocity = 5 # how fast character moves
         self.health = 20
@@ -30,8 +30,11 @@ class Player(pygame.sprite.Sprite): # class inherits from the sprite class to be
             self.direction.y = 0
 
     def update(self):
+        old_rect = self.rect.copy() # holds a copy of the current rect before movement, in case of a collision with another object
         self.input()
-        self.rect.center += self.direction * self.velocity
+        if self.direction.magnitude() != 0: # magnitude is absolute length of the vector given x and y (a^2 + b^2 = c^2)
+            self.direction = self.direction.normalize() # normalize sets the magnitude to 1, so if two directions are pressed, the vector will be normalized to 1 instead of sqrt(2) 
+        self.rect.center += self.direction * self.velocity # rect.center is a tuple and vector2's are compatable with operations involving tuples'
 
 def main():
     print("Wrong file: please run run python3 main.py")
