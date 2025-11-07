01be58b65bea2eee056ba96bd792b844f0d969d3
Yukon3x7 <777benjilin@gmail.com>
2025-11-07 16:58:58 -0500

    sprite updates

--- ch09/final/src/button.py
+++ ch09/final/src/button.py
@@ -3,7 +3,9 @@ import pygame
 class Button(pygame.sprite.Sprite): # class inherits from the sprite class to be able to add it to a sprite group and use update() and draw()
     def __init__(self, pos, group):
         super().__init__(group) # adds player sprite into the camera_group sprite group calling the sprite constructor Sprite(self, group)
-        self.image = pygame.image.load('assets/placeholder_assets/small_button.png')
+        self.image = pygame.image.load('assets/assets_UI/start_button.webp')
+        w,h = self.image.get_size()
+        self.image = pygame.transform.scale(self.image, (2*w,2*h))
         self.rect = self.image.get_rect(center = pos) # image_group needs an image and a rect in order to use it's built in functions draw() and update()
         self.pressed = False
     
