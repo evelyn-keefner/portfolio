af3ab90c09ee7373983fda0864a2c0ec0e94973e
Yukon3x7 <777benjilin@gmail.com>
2025-11-14 14:57:44 -0500

    start button

--- ch09/final/src/button.py
+++ ch09/final/src/button.py
@@ -3,12 +3,11 @@ import pygame
 class Button(pygame.sprite.Sprite): # class inherits from the sprite class to be able to add it to a sprite group and use update() and draw()
     def __init__(self, pos, group, image, text):
         super().__init__(group) # adds player sprite into the camera_group sprite group calling the sprite constructor Sprite(self, group)
-        self.image = pygame.image.load('assets/assets_ui/start_button.webp')
+        self.image = pygame.image.load(image)
         w,h = self.image.get_size()
         self.image = pygame.transform.scale(self.image, (2*w,2*h))
         self.display_surface = pygame.display.get_surface()
         self.font = pygame.font.SysFont('Arial', 25)
-        self.image = pygame.image.load(image)
         self.text = text
         self.rect = self.image.get_rect(center = pos) # image_group needs an image and a rect in order to use it's built in functions draw() and update()
         self.pressed = False
