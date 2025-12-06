6fca05410b22c0230dffa3f2eb73a86a37034128
evelyn-keefner <evelyn0keefner@gmail.com>
2025-12-05 20:54:00 -0500

    health and ending screen

--- ch09/final/src/enemy.py
+++ ch09/final/src/enemy.py
@@ -46,9 +46,8 @@ class Enemy(pygame.sprite.Sprite): # class inherits from the sprite class to be 
                 if self.direction.magnitude() != 0: # magnitude is absolute length of the vector given x and y (a^2 + b^2 = c^2)
                     self.direction = self.direction.normalize() # normalize sets the magnitude to 1, so if two directions are pressed, the vector will be normalized to 1 instead of sqrt(2) 
 
-            if self.frame % 25 == 0:
-                if self.direction.x < 0:
-                    self.image = pygame.transform.flip(self.image, True, False)
+            if self.direction.x < 0:
+                self.image = pygame.transform.flip(self.image, True, False)
 
                 if self.health <= 0:
                     xp = Experience(self.rect.center, self.camera_group, self.experience) # pos, group, value
